3b419faaa52289413111d3ea520c3ff1a2a3a2b6
xfold <xfaran@hotmail.com>
2024-08-13 23:51:37 +0100

    created python .py script to run with args

---
+++ process_images.py
@@ -0,0 +1,17 @@
+import argparse
+from image_date_processor import ImageProcessor
+
+def main():
+    parser = argparse.ArgumentParser(description='Process images with EXIF data.')
+    parser.add_argument('folder_path', type=str, help='Path to the folder containing images.')
+    parser.add_argument('folder_path_exif', type=str, help='Path to the folder to save processed images.')
+    parser.add_argument('filters', type=str, nargs='+', help='List of image file extensions to filter by.')
+    parser.add_argument('--verbose', action='store_true', help='Enable verbose output.')
+
+    args = parser.parse_args()
+
+    processor = ImageProcessor()
+    processor.process_images_with_exif(args.folder_path, args.folder_path_exif, args.filters, verbose=args.verbose)
+
+if __name__ == '__main__':
+    main()
